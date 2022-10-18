9d3ed5aab8faecb483ec1c387f673b230f958356
Lucas Ramos <ramos.lucash@gmail.com>
2022-10-17 23:29:04 -0300

    feat: setup vite apps as internal dependencies

---
+++ apps/app2/src/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>App 2</h1>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { App };
